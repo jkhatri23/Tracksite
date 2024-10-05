3b677a0e4928d63783508b5129675c304af35f3c
jkhatri23 <jordankhatri23@gmail.com>
2024-10-05 18:11:06 -0400

    frontend

---
+++ tracksite-frontend/src/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const About = () => {
+  return (
+    <header className="App-header">
+    <div className="maintext">
+        
+        <ul><h1>About Us</h1> </ul>
+        <p> Easily save, organize, and access all your favorite websites with our bookmark manager.</p>
+          <p>Whether you're storing research, recipes, articles, or entertainment, this platform helps you categorize and retrieve links with just a click. </p>
+          <p>Customize folders to find exactly what you need — whenever you need it.</p>
+    </div>
+    </header>
+  );
+};
+
+export default About;
